d4e0f45dcd61317146eb1f12bdd3d51e4f89cebe
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-11-19 16:35:04 +0100

    Created quick How it works-section. Just basc structure for now, image and text will be adjusted later.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import BestSellerSection from "./components/BestSellerSection";
 import HeaderImage from "./components/HeaderImage";
+import HowItWorks from "./components/HowItWorks";
 
 export default function Home() {
   return (
     <>
       <HeaderImage />
       <BestSellerSection />
+      <HowItWorks />
     </>
   );
 }
