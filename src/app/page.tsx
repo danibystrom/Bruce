8fc2b951ef51cc5184c2bba9b80103a7fcb83cad
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-11-19 17:34:33 +0100

    Added ProductSection to startpage showing a scroll with products with a button which will navigate users to a product-page in the future.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import BestSellerSection from "./components/BestSellerSection";
 import HeaderImage from "./components/HeaderImage";
 import HowItWorks from "./components/HowItWorks";
+import ProductsSection from "./components/ProductsSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeaderImage />
       <BestSellerSection />
       <HowItWorks />
+      <ProductsSection />
     </>
   );
 }
