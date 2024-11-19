d8b7816e7de017a96dd3613a7e179e90d099c074
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-11-19 11:39:09 +0100

    Added isBestseller boolean in mock data, and created BestSellerSection-file.

--- src/app/data/drinks.ts
+++ src/app/data/drinks.ts
@@ -5,6 +5,7 @@ export interface Drink {
   ingredients: string[];
   image: string;
   price: number;
+  isBestSeller?: boolean;
 }
 
 export const drinks: Drink[] = [
@@ -16,6 +17,7 @@ export const drinks: Drink[] = [
     ingredients: ["Tequila", "Fresh Lime Juice", "Agave Syrup", "Salt"],
     image: "/drink.jpeg",
     price: 3.5,
+    isBestSeller: true,
   },
   {
     id: 2,
@@ -25,32 +27,36 @@ export const drinks: Drink[] = [
     ingredients: ["Tequila", "Fresh Lime Juice", "Agave Syrup", "Salt"],
     image: "/drink.jpeg",
     price: 3.5,
+    isBestSeller: true,
   },
   {
-    id: 2,
+    id: 3,
     name: "Margarita",
     description:
       "A timeless classic with a twist. This vibrant cocktail blends the zesty freshness of lime, the smooth richness of tequila, and a touch of orange liqueur, all balanced by a salted rim. Perfectly refreshing, effortlessly chic, and always a crowd-pleaser. The Margarita is your go-to for sun-kissed moments and unforgettable nights.",
     ingredients: ["Tequila", "Fresh Lime Juice", "Agave Syrup", "Salt"],
     image: "/drink.jpeg",
     price: 3.5,
+    isBestSeller: true,
   },
   {
-    id: 2,
+    id: 4,
     name: "Margarita",
     description:
       "A timeless classic with a twist. This vibrant cocktail blends the zesty freshness of lime, the smooth richness of tequila, and a touch of orange liqueur, all balanced by a salted rim. Perfectly refreshing, effortlessly chic, and always a crowd-pleaser. The Margarita is your go-to for sun-kissed moments and unforgettable nights.",
     ingredients: ["Tequila", "Fresh Lime Juice", "Agave Syrup", "Salt"],
     image: "/drink.jpeg",
     price: 3.5,
+    isBestSeller: true,
   },
   {
-    id: 2,
+    id: 5,
     name: "Margarita",
     description:
       "A timeless classic with a twist. This vibrant cocktail blends the zesty freshness of lime, the smooth richness of tequila, and a touch of orange liqueur, all balanced by a salted rim. Perfectly refreshing, effortlessly chic, and always a crowd-pleaser. The Margarita is your go-to for sun-kissed moments and unforgettable nights.",
     ingredients: ["Tequila", "Fresh Lime Juice", "Agave Syrup", "Salt"],
     image: "/drink.jpeg",
     price: 3.5,
+    isBestSeller: true,
   },
 ];
