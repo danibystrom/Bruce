4094ab84751f29f396adfd56d010aabb46721bf7
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-11-24 16:43:38 +0100

    Some quick styling on cart-items and order summary. Right now we only mapping throug all products for some visuals, no add-to-cart-functions implemented yet.

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,6 +5,7 @@ import CloseIcon from "@mui/icons-material/Close";
 import RemoveIcon from "@mui/icons-material/Remove";
 import {
   Box,
+  Button,
   CardContent,
   Divider,
   Grid,
@@ -23,24 +24,33 @@ export default function CheckoutPage() {
           container
           justifyContent="center"
           alignItems="flex-start"
-          spacing={0.5}
+          spacing={2}
           sx={{
             padding: "1rem",
-            bgcolor: "#FAFAFB",
+            bgcolor: "#fff",
             minHeight: "100vh",
-            paddingTop: { xs: "97px", sm: "104px", md: "104px" },
+            width: "100%",
           }}
         >
           {/* Products Column */}
-          <Grid item xs={12} md={8}>
+          <Grid item xs={12} md={9}>
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "100",
+                marginBottom: "1rem",
+                fontSize: { xs: "1.5rem", md: "2rem" },
+              }}
+            >
+              My cart
+            </Typography>
+            <Divider sx={{ marginBottom: "1rem", borderColor: "#000" }} />
+
             {product.map((item) => (
               <Box
                 key={item.id}
                 sx={{
-                  padding: "1rem",
                   backgroundColor: "#fff",
-                  border: "1px solid #f6f5f3",
-                  // height: "300px",
                 }}
               >
                 <Grid container alignItems="center" spacing={2}>
@@ -50,13 +60,14 @@ export default function CheckoutPage() {
                       alt={item.name}
                       style={{
                         width: "100%",
-                        height: "relative",
+                        height: "120px",
+                        objectFit: "cover",
                         marginBottom: "1rem",
                       }}
                     />
                   </Grid>
                   <Grid item xs={12} sm={9}>
-                    <CardContent>
+                    <CardContent sx={{ padding: "0.5rem" }}>
                       <Box
                         sx={{
                           display: "flex",
@@ -67,7 +78,7 @@ export default function CheckoutPage() {
                         <Typography
                           variant="h6"
                           sx={{
-                            fontSize: "1.5rem",
+                            fontSize: "1.2rem",
                             fontWeight: "500",
                           }}
                         >
@@ -77,21 +88,12 @@ export default function CheckoutPage() {
                           <CloseIcon sx={{ fontSize: "1rem" }} />
                         </IconButton>
                       </Box>
+
                       <Typography
                         variant="body2"
                         color="text.secondary"
                         sx={{
-                          fontSize: "1rem",
-                          color: "#797979",
-                        }}
-                      >
-                        Storlek: {item.ingredients}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        sx={{
-                          fontSize: "1rem",
+                          fontSize: "0.9rem",
                           marginBottom: "0.5rem",
                         }}
                       >
@@ -140,72 +142,82 @@ export default function CheckoutPage() {
                     </CardContent>
                   </Grid>
                 </Grid>
+                <Divider sx={{ marginBottom: "1rem", borderColor: "#000" }} />
               </Box>
             ))}
           </Grid>
 
-          {/* Contact Form Column */}
-          <Grid item xs={12} md={4}>
+          {/* Summary Column */}
+          <Grid item xs={12} md={3}>
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "100",
+                marginBottom: "1rem",
+                fontSize: { xs: "1.5rem", md: "2rem" },
+              }}
+            >
+              Order summary
+            </Typography>
+            <Divider sx={{ marginBottom: "1rem", borderColor: "#000" }} />
+
             <Box
               sx={{
                 width: "100%",
                 backgroundColor: "#fff",
-                padding: "1rem",
               }}
             >
-              <CardContent>
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontWeight: "100",
-                    marginBottom: "1rem",
-                    fontSize: { xs: "1.5rem", md: "2rem" },
-                  }}
-                >
-                  Din varukorg ({})
-                </Typography>
-
-                {/* Summary with Left and Right Aligned Text */}
-                <Grid container justifyContent="space-between">
-                  <Grid item>
-                    <Typography variant="body1">Summa:</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="body1">TOTAL €</Typography>
-                  </Grid>
+              <Grid container justifyContent="space-between">
+                <Grid item>
+                  <Typography variant="body1">Subtotal</Typography>
                 </Grid>
-                <Grid container justifyContent="space-between">
-                  <Grid item>
-                    <Typography variant="body1">Frakt:</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="body1">49 kr</Typography>
-                  </Grid>
+                <Grid item>
+                  <Typography variant="body1">TOTAL €</Typography>
                 </Grid>
-                <Grid container justifyContent="space-between">
-                  <Grid item>
-                    <Typography variant="body1">Att betala:</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="body1">SHIPPING €</Typography>
-                  </Grid>
+              </Grid>
+              <Grid container justifyContent="space-between">
+                <Grid item>
+                  <Typography variant="body1">Estimated Delivery</Typography>
                 </Grid>
+                <Grid item>
+                  <Typography variant="body1">EUR 105€</Typography>
+                </Grid>
+              </Grid>
+              <Grid container justifyContent="space-between">
+                <Grid item>
+                  <Typography variant="body1">Total</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body1">SHIPPING €</Typography>
+                </Grid>
+              </Grid>
 
-                <Divider />
-                <Typography
-                  variant="h6"
-                  sx={{
-                    fontWeight: "100",
-                    marginTop: "1rem",
-                    fontSize: { xs: "1.5rem", md: "2rem" },
-                  }}
-                >
-                  Leveransuppgifter
-                </Typography>
-
-                {/* <CustomerForm /> */}
-              </CardContent>
-              <Divider />
+              <Divider sx={{ marginBottom: "1rem", borderColor: "#000" }} />
+              <Button
+                variant="outlined"
+                sx={{
+                  width: "100%",
+                  borderRadius: "20px",
+                  borderColor: "#000",
+                  color: "#000",
+                }}
+              >
+                Checkout
+              </Button>
+              <Button
+                variant="outlined"
+                sx={{
+                  width: "100%",
+                  borderRadius: "20px",
+                  border: "none",
+                  backgroundColor: "#FFC33A",
+                  color: "#049CDE",
+                  marginTop: "0.5rem",
+                  fontWeight: "bold",
+                }}
+              >
+                PayPal
+              </Button>
             </Box>
           </Grid>
         </Grid>
