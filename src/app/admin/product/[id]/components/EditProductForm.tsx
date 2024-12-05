aa58e66a96c7fbb68c74e03d5ac96c13cc324593
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-12-05 15:01:14 +0100

    Small styling on admin/product-page

--- src/app/admin/product/[id]/components/EditProductForm.tsx
+++ src/app/admin/product/[id]/components/EditProductForm.tsx
@@ -1,3 +1,4 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
@@ -5,47 +6,86 @@ export default function EditProductForm() {
   return (
     <Box
       component="form"
-      sx={{ "& .MuiTextField-root": { m: 1, width: "50%" } }}
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "100%" },
+        "& .MuiInputBase-input": { color: "#000", fontSize: "0.8rem" },
+        "& .MuiInput-underline:before": { borderBottomColor: "#000" },
+        "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+          borderBottomColor: "#000",
+        },
+        "& .MuiInput-underline:after": { borderBottomColor: "#000" },
+        "& .MuiFormLabel-root": { color: "#000" },
+        "& .MuiFormLabel-root.Mui-focused": { color: "#000" },
+        "& .MuiSelect-select": { color: "#000" },
+        "& .MuiOutlinedInput-notchedOutline": { borderColor: "#000" },
+        "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+          borderColor: "#000",
+        },
+        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+          {
+            borderColor: "#000",
+          },
+      }}
       noValidate
       autoComplete="off"
     >
       <div>
         <TextField
-          label="Size"
+          label="Name"
+          id="standard-size-small"
+          size="small"
+          variant="standard"
+        />
+
+        <TextField
+          label="Ingredients"
           id="standard-size-small"
           size="small"
           variant="standard"
         />
         <TextField
-          label="Size"
+          label="Alc. %"
           id="standard-size-small"
           size="small"
           variant="standard"
         />
         <TextField
-          label="Size"
+          label="Price"
           id="standard-size-small"
           size="small"
           variant="standard"
         />
         <TextField
-          label="Size"
-          id="standard-size-small"
-          size="small"
+          id="standard-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
           variant="standard"
         />
-        <TextField
-          label="Size"
-          id="standard-size-small"
-          size="small"
-          variant="standard"
-        />
-        <TextField
-          label="Size"
-          id="standard-size-small"
-          size="small"
-          variant="standard"
-        />
+        <FormControl variant="standard" sx={{ m: 1, width: "100%" }}>
+          <InputLabel
+            sx={{
+              color: "#000",
+              "&.Mui-focused": { color: "#000" },
+            }}
+            id="category-label"
+          >
+            Category
+          </InputLabel>
+          <Select
+            label="Category"
+            labelId="category-label"
+            id="category"
+            defaultValue=""
+            sx={{
+              color: "#000",
+            }}
+          >
+            <MenuItem value={10}>Option 1</MenuItem>
+            <MenuItem value={20}>Option 2</MenuItem>
+            <MenuItem value={30}>Option 3</MenuItem>
+          </Select>
+        </FormControl>
       </div>
     </Box>
   );
--- src/app/admin/product/[id]/page.tsx
+++ src/app/admin/product/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { Box, Typography } from "@mui/material";
+"use client";
+import { Box, Button, Divider, Typography } from "@mui/material";
+import Link from "next/link";
 import EditProductForm from "./components/EditProductForm";
 
 type Props = { params: { id: string } };
@@ -9,23 +11,128 @@ export default function EditProductPage({ params }: Props) {
   return (
     <Box
       sx={{
-        paddingBottom: "55px",
+        paddingBottom: "20x",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#fff",
       }}
     >
       <Typography
-        variant="h4"
+        variant="h6"
         sx={{
-          padding: "20px",
-          textAlign: "center",
-          fontSize: "2rem",
-          fontWeight: "700",
+          fontWeight: "100",
+          marginBottom: "1rem",
+          fontSize: { xs: "1.5rem", md: "2rem" },
         }}
       >
         Edit Product with id: {productId}
       </Typography>
+      <Divider
+        sx={{
+          width: "70%",
+          borderColor: "#000",
+          marginBottom: "1rem",
+        }}
+      />
 
-      <Box sx={{ padding: "1rem", maxWidth: "500px", margin: "0 auto" }}>
-        <EditProductForm />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#fff",
+          borderRadius: "8px",
+          padding: "20px",
+          width: "80%",
+          maxWidth: "1000px",
+        }}
+      >
+        <Box sx={{ flex: 1, paddingRight: { md: "20px" } }}>
+          <EditProductForm />
+        </Box>
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{
+            margin: { xs: "20px 0", md: "0 20px" },
+            borderColor: "#000",
+          }}
+        />
+
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <img
+            src="./public/bruce-case.png"
+            alt="Product"
+            style={{
+              maxWidth: "100%",
+              maxHeight: "300px",
+              objectFit: "contain",
+              borderRadius: "8px",
+            }}
+          />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          width: "80%",
+          maxWidth: "1000px",
+          marginTop: "20px",
+          gap: "1rem",
+        }}
+      >
+        <Link href="" style={{ textDecoration: "none", width: "150px" }}>
+          <Button
+            disableRipple
+            variant="outlined"
+            size="small"
+            sx={{
+              borderRadius: "20px",
+              color: "#000",
+              width: "100%",
+              marginRight: "0.8rem",
+              backgroundColor: "transparent",
+              borderColor: "#000",
+              "&:hover": { backgroundColor: "#000", color: "#fff" },
+            }}
+          >
+            Save changes
+          </Button>
+        </Link>
+        <Link href="/admin" style={{ textDecoration: "none", width: "150px" }}>
+          <Button
+            disableRipple
+            variant="outlined"
+            size="small"
+            sx={{
+              borderRadius: "20px",
+              backgroundColor: "#000",
+              width: "100%",
+              color: "#fff",
+              "&:hover": {
+                backgroundColor: "#fff",
+                color: "#000",
+                boxShadow: "none",
+                borderColor: "#000",
+              },
+            }}
+          >
+            Cancel
+          </Button>
+        </Link>
       </Box>
     </Box>
   );
