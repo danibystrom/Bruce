9e9133b15ffccde9672fa61f2a05b0b3f334b451
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-12-29 18:36:55 +0100

    Change of layout in header image

--- src/app/components/HeaderImage.tsx
+++ src/app/components/HeaderImage.tsx
@@ -1,53 +1,77 @@
 "use client";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 export default function HeaderImage() {
   return (
-    <Box
-      sx={{
-        position: "relative",
-        width: "98%",
-        height: "90vh",
-        margin: "auto",
-        borderRadius: 0,
-        overflow: "hidden",
-      }}
-    >
-      <Box
-        sx={{
-          backgroundImage: 'url("/hero-img.jpeg")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          width: "100%",
-          height: "100%",
-        }}
-      />
+    <Box sx={{ width: "98%", margin: "auto", overflow: "hidden" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={7}>
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              backgroundImage: 'url("/hero-img.jpeg")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: 0,
+              minHeight: "450px",
+            }}
+          />
+        </Grid>
 
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          color: "#ffffff",
-          textAlign: "center",
-        }}
-      >
+        <Grid item xs={12} md={5}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              height: "100%",
+              overflow: "hidden",
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: {
+                  xs: "2rem",
+                  sm: "4rem",
+                  md: "8rem",
+                  lg: "13rem",
+                },
+                fontWeight: 700,
+                lineHeight: 0.71,
+              }}
+            >
+              bruce
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Box sx={{ mb: 1, textAlign: "left" }}>
         <Typography
-          variant="h1"
+          variant="subtitle1"
           sx={{
-            fontSize: {
-              xs: "8rem",
-              sm: "20rem",
-              md: "23rem",
-              lg: "30rem",
-              xl: "35rem",
-            },
-            fontWeight: 700,
-            opacity: 0.7,
+            fontSize: { xs: "0.9rem", sm: "1rem" },
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            marginTop: "1rem",
+            mb: 1,
           }}
         >
-          BRUCE
+          Delicious Cocktails, No Bar Required
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            fontSize: { xs: "1rem", sm: "1.2rem" },
+            maxWidth: "700px",
+            marginBottom: "2rem",
+          }}
+        >
+          He’s all you need to craft the world’s best drinks. One tablet, some
+          water, and you’re good to go. Bruce thinks smart, packs light, and
+          always makes sure the party’s with you – wherever you are.
         </Typography>
       </Box>
     </Box>
