3640b6f8a7908ebe737036dd40ca3702e83fb979
danibystrom <124156950+danibystrom@users.noreply.github.com>
2025-01-03 11:43:22 +0100

    Added new hero image, and layout to about us page

---
+++ src/app/about/components/AboutUsHeader.tsx
@@ -0,0 +1,117 @@
+"use client";
+import { Box, Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+export default function ProductsPageHero() {
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+
+  return isTablet ? (
+    <Box
+      sx={{
+        width: "98%",
+        margin: "auto",
+        overflow: "hidden",
+      }}
+    >
+      <Box
+        sx={{
+          position: "relative",
+          width: "98%",
+          height: "40vh",
+          margin: "auto",
+          borderRadius: 0,
+          overflow: "hidden",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundImage: 'url("/about-us-hero.jpg")',
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "100%",
+            height: "100%",
+          }}
+        />
+
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            color: "#ffffff",
+            textAlign: "center",
+            width: "100%",
+            maxWidth: "90%",
+            padding: "0 1rem",
+          }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: {
+                xs: "5em",
+                sm: "6rem",
+              },
+              lineHeight: 0.9,
+              fontWeight: 400,
+              fontFamily: "'Instrument Serif', sans-serif !important",
+              whiteSpace: "normal",
+              wordWrap: "break-word",
+            }}
+          >
+            The story of Bruce
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  ) : (
+    <Box sx={{ width: "98%", margin: "auto", overflow: "hidden" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={7}>
+          <Box
+            sx={{
+              width: "100%",
+              height: "40vh",
+              backgroundImage: 'url("/about-us-hero.jpg")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: 0,
+              minHeight: "450px",
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={5}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              height: "100%",
+              overflow: "hidden",
+            }}
+          >
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: {
+                  xs: "2rem",
+                  sm: "4rem",
+                  md: "7rem",
+                  lg: "9rem",
+                },
+                fontWeight: 400,
+                lineHeight: 0.8,
+                fontFamily: "'Instrument Serif', sans-serif !important",
+              }}
+            >
+              The story of Bruce
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
