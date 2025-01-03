20ed8be5a7ae4c9d35798a4efd4bd12dd90214f0
danibystrom <124156950+danibystrom@users.noreply.github.com>
2025-01-03 16:03:07 +0100

    Added &apos;

--- src/app/sustainability/components/AboutSection.tsx
+++ src/app/sustainability/components/AboutSection.tsx
@@ -40,25 +40,35 @@ export default function AboutSection() {
           variant="body1"
           sx={{ marginBottom: 2, fontSize: { xs: "0.9rem", sm: "1rem" } }}
         >
-          Bruce isn't just about great taste—it's about making life easier and more sustainable. We’ve reinvented the way you enjoy drinks: just grab a case, then refill with our eco-friendly tablets. No waste, no hassle, just a perfectly refreshing sip every time. 
+          Bruce isn't just about great taste—it's about making life easier and
+          more sustainable. We&apos;ve reinvented the way you enjoy drinks: just
+          grab a case, then refill with our eco-friendly tablets. No waste, no
+          hassle, just a perfectly refreshing sip every time.
         </Typography>
         <Typography
           variant="body1"
           sx={{ marginBottom: 2, fontSize: { xs: "0.9rem", sm: "1rem" } }}
         >
-          Our commitment? To reduce single-use plastics and make sustainability a part of every sip. With Bruce, all you need is one case, and you’re good to go—saving the planet, one refill at a time.
+          Our commitment? To reduce single-use plastics and make sustainability
+          a part of every sip. With Bruce, all you need is one case, and
+          you&apos;re good to go—saving the planet, one refill at a time.
         </Typography>
         <Typography
           variant="body1"
           sx={{ marginBottom: 2, fontSize: { xs: "0.9rem", sm: "1rem" } }}
         >
-          At Bruce, we believe that small actions lead to big changes. That’s why we’ve designed a system that allows you to keep enjoying your favorite drinks, without the guilt of excess waste. Our mission is to make sustainability as simple as cracking open a cold one.
+          At Bruce, we believe that small actions lead to big changes.
+          That&apos;s why we&apos;ve designed a system that allows you to keep
+          enjoying your favorite drinks, without the guilt of excess waste. Our
+          mission is to make sustainability as simple as cracking open a cold
+          one.
         </Typography>
         <Typography
           variant="body1"
           sx={{ marginBottom: 2, fontSize: { xs: "0.9rem", sm: "1rem" } }}
         >
-          So, join us in our quest for a greener future. Sip, refill, and repeat—the planet will thank you.
+          So, join us in our quest for a greener future. Sip, refill, and
+          repeat—the planet will thank you.
         </Typography>
       </Grid>
     </Box>
